2ac397d7eb9b74ec7d5402a6e89a006708e09009
Shreysi <shreysi.swaraj.2000@gmail.com>
2024-06-20 15:52:21 +0530

    BMI-Calculator

---
+++ BMI-Generator/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form'); 
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    
+        if(height === ' ' ||  height < 0 || isNaN(height)) {
+            results.innerHTML = `Please give a valid height`;
+        } 
+        else if(weight === ' ' ||  weight < 0 || isNaN(weight)){
+            results.innerHTML = `Please give a valid weight`;
+        }
+        else {
+            const bmi = (weight / (height * height/10000)).toFixed(2);
+            console.log(bmi);
+            if(bmi < 18.6){
+                results.innerHTML = `<span> ${bmi} UnderWeight</span>`;
+            }
+            else if(bmi > 18.6 && bmi< 24){
+                results.innerHTML = `<span> ${bmi} Normal Weight</span>`;
+            }
+            else{
+                results.innerHTML = `<span> ${bmi} Over Weight</span>`;
+            }
+
+
+        }
+    
+    
+});
+
